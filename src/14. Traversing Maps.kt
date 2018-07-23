352581205b2f9d666f2d8fdbc761fd009648e99e
Aditya Sharat <adityasharat@gmail.com>
2018-07-23 19:26:54 +0530

    add lesson 14

---
+++ src/14. Traversing Maps.kt
@@ -0,0 +1,14 @@
+/**
+ *  Kotlin Standard Library provide component functions for Map.Entry
+ */
+
+fun main(args: Array<String>) {
+    val map = hashMapOf<String, Int>()
+    map["one"] = 1
+    map["two"] = 2
+    map["three"] = 3
+
+    for ((key, value) in map) {
+        println("key = $key, value = $value")
+    }
+}
