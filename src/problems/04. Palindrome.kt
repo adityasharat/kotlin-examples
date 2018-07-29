411717781d498df2b726610131855cdbda51b3c3
Aditya Sharat <adityasharat@gmail.com>
2018-07-29 13:09:52 +0530

    add problem 4

---
+++ src/problems/04. Palindrome.kt
@@ -0,0 +1,14 @@
+/*
+ * Your task is to implement a palindrome test.
+ *
+ * A string is called a palindrome when it reads the same way left-to-right
+ * and right-to-left.
+ *
+ * See http://en.wikipedia.org/wiki/Palindrome
+ */
+package problems
+
+fun isPalindrome(input: String): Boolean {
+    return input == input.reversed()
+}
+
